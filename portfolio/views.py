cd30250956e5cb44ee3848cfefa6c72465451241
titans55 <kutayaslanfb@hotmail.com>
2019-03-21 01:00:37 +0300

    csrfFix

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 def landingPage(request):
 
     return render(request, 'landingPage.html')
-
+    
+@csrf_exempt
 def verifyFriend(request):
     request.session['isFriend'] = True
     return HttpResponse("True")
