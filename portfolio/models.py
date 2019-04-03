d0d1896d8b0582d7f3405a5450f2ca3442b93dc4
titans55 <kutayaslanfb@hotmail.com>
2019-04-03 12:22:39 +0300

    changed time zone

--- portfolio/models.py
+++ portfolio/models.py
@@ -19,7 +19,7 @@ class Hit(models.Model):
     bot = models.BooleanField(null=True)
 
     def __str__(self):
-        return str(self.time)
+        return str(self.device_ip)
 
 class Experience(models.Model):
     header = models.CharField(max_length=200, null=False)
